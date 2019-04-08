9ebb47e35d2f06be691e8615e1edf81002eeb26c
LucasFerreiraB <47697686+LucasFerreiraB@users.noreply.github.com>
2019-04-08 20:42:50 -0300

    Atualização

--- banco/banco.sql
+++ banco/banco.sql
@@ -1,40 +1,34 @@
-CREATE TABLE Mensagem (
-Id_Mensagem Texto(1) PRIMARY KEY,
-Telefone Texto(1),
-Nome_Cliente Texto(1),
-Email Texto(1),
-Mensagem Texto(1)
+CREATE TABLE Usuário (
+Id_Usuário INT PRIMARY KEY,
+Senha VARCHAR(50),
+E-mail VARCHAR(50)
 )
 
 CREATE TABLE Fotos (
-Id_Fotos Texto(1) PRIMARY KEY,
-Galeria_3 Texto(1),
-Galeira_1 Texto(1),
-Galeira_2 Texto(1)
+Id_Fotos INT PRIMARY KEY,
+Imagem VARCHAR(100),
+Id_Produto INT
+)
+
+CREATE TABLE Mensagem (
+Id_Mensagem INT PRIMARY KEY,
+Telefone VARCHAR(100),
+Nome_Cliente VARCHAR(100),
+E-mail VARCHAR(100),
+Mensagem VARCHAR(2000),
+Id_Produto INT
 )
 
 CREATE TABLE Produto (
-Id_Produto Texto(1) PRIMARY KEY,
-Nome_Produto Texto(1),
-Tamanho_Total Texto(1),
-Material_Lamina Texto(1),
-Comprimento_Lamina Texto(1),
-Largura_Lamina Texto(1),
-Material_Cabo Texto(1),
-Dorso Texto(1),
-Cor Texto(1),
-Inserido_Por Texto(1),
-Id_Fotos Texto(1),
-Id_Mensagem Texto(1),
-FOREIGN KEY(Id_Fotos) REFERENCES Fotos (Id_Fotos),
-FOREIGN KEY(Id_Mensagem) REFERENCES Mensagem (Id_Mensagem)
+Id_Produto INT PRIMARY KEY,
+Nome_Produto VARCHAR(100),
+Altura FLOAT,
+Largura FLOAT,
+Cor VARCHAR(100),
+Comprimento FLOAT,
+Id_Usuário INT,
+FOREIGN KEY(Id_Usuário) REFERENCES Usuário (Id_Usuário)
 )
 
-CREATE TABLE Usuário (
-Id_Usuário Texto(1) PRIMARY KEY,
-Senha Texto(1),
-Nome_Usuário Texto(1),
-Email Texto(1),
-Id_Produto Texto(1),
-FOREIGN KEY(Id_Produto) REFERENCES Produto (Id_Produto)
-)
+ALTER TABLE Fotos ADD FOREIGN KEY(Id_Produto) REFERENCES Produto (Id_Produto)
+ALTER TABLE Mensagem ADD FOREIGN KEY(Id_Produto) REFERENCES Produto (Id_Produto)
