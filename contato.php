ebd12b93f80b650211421a65dceefb9247c9042b
LucasFerreiraB <47697686+LucasFerreiraB@users.noreply.github.com>
2019-04-08 21:53:53 -0300

    Update contato.php

--- contato.php
+++ contato.php
@@ -20,22 +20,30 @@
       <?php include "menu.php";
       ?>
         <br>
-        <h1>Entre em contato</h1><br>
-        <input type="hidden" name="acao" value="cadastrar">
+        <h1 class="text-center">Entre em contato</h1><br>
+		<div class="container">
         <div class="form-group">
-         <label>Nome</label>
-        <input type="text" name="nome_funcionario" class="form-control">
+			<label>Nome</label>
+			<input type="text" name="nome" class="form-control" required> 
         </div>
-         <div class="form-group">
-          <label>E-mail</label>
-         <input type="email" name="email_funcionario" class="form-control">
-         </div>
-         <div class="form-group">
+		<div class="row">	 
+		<div class="form-group col-6">
+			  <label>E-mail</label>
+			 <input type="email" name="email" class="form-control" required>
+        </div>
+         <div class="form-group col-6">
            <label>Fone</label>
-            <input type="text" name="fone_funcionario" class="form-control">
+            <input type="text" name="fone" class="form-control" required>
          </div>
-         <button type="submit" class="btn btn-primary">Enviar</button>
-
+		 </div>
+		<div class="form-group">
+			<textarea class="form-control" id="descricao" name="descricao" rows="5" cols="20" maxlength="500" placeholder="Envie uma mensagem!"></textarea>
+			</div>
+		
+			<div class="form-group">
+         <button type="submit" class="btn btn-primary mr-auto">Enviar</button>
+		 </div>
+</div>
 
         <?php include "footer.php";
       ?>
@@ -46,3 +54,4 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
+
